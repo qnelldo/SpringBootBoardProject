6e05e405816d8274b0c74c71bd848f197f08377c
qnelldo <131656049+qnelldo@users.noreply.github.com>
2023-05-26 18:12:26 +0900

    1st

---
+++ README.md
@@ -0,0 +1,7 @@
+# 게시판
+### 1. 글쓰기 (/board/write)
+### 2. 글목록 (/board/list)
+### 3. 글조회 (/board/{id})
+### 4. 글수정 (/board/update/{id})
+### 5. 글삭제 (/board/delete/{id})
+### 6. 페이징처리 (/board/paging/)
---
+++ src/main/java/com/example/boardproject/controller/HomePageController.java
@@ -0,0 +1,15 @@
+package com.example.boardproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+    }
+}
